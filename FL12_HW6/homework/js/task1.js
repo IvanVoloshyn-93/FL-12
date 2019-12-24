7c9154ae72683b1874e3f137d511e307164e1920
Ivan Voloshyn <voloshynivan1993@gmail.com>
2019-12-24 22:01:15 +0200

    triangle edit

---
+++ FL12_HW6/homework/js/task1.js
@@ -0,0 +1,20 @@
+let a = +prompt("Enter number 'a'");
+let b = +prompt("Enter number 'b'");
+let c = +prompt("Enter number 'c'");
+if (a === 0) {
+    console.log('Invalid input for *a*');
+}
+if (isNaN(a || b || c) === true) {
+    console.log('Invalid input data');
+} 
+let D = b * b - 4 * a * c;
+if (D === 0) {
+    let x = -b / 2 * a;
+    console.log('x = ' + x);
+} else if (D > 0) {
+    let x1 = (-b + Math.sqrt(D)) / (2 * a);
+    let x2 = (-b - Math.sqrt(D)) / (2 * a);
+    console.log('x1 = ' + x1 + ' x2 = ' + x2);
+} else {
+    console.log('no solution');
+}
